2be7e004593dfd8c07bd105d9470b5a530aa6dba
kang01 <904451260@qq.com>
2015-12-01 17:44:22 +0800

    构建项目完成

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularBootstrapApp
+ * @description
+ * # angularBootstrapApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularBootstrapApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootstrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBootstrapApp
+ */
+angular.module('angularBootstrapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
